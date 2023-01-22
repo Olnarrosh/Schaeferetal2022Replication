feab80232e67875a8d156d87e3a7a00365439e13
Felix Eberhardt <feleber@yahoo.com>
2023-01-23 00:39:03 +0100

    embeddings

--- corpus2embeddings.py
+++ corpus2embeddings.py
@@ -1,12 +1,17 @@
-from transformers import AutoTokenizer
+from transformers import BertTokenizer, BertModel
+from torch import tensor, cuda
 
-tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+model = BertModel.from_pretrained("bert-base-uncased")
 
-def convert_corpus(corpus: list[(list[str], bool)]) -> list[(dict, bool)]:
+def convert_corpus(corpus: list[(list[str], bool)]) -> list[(list[float], bool)]:
     sentences = []
     for sentence, is_claim in corpus:
         tokenized = tokenizer(sentence)
-        tokens = tokenized["input_ids"]
-        pooled = round(sum(tokens)/len(tokens))
-        sentences.append(({"input_ids": [pooled], "attention_mask": [1]}, is_claim))
-    return sentences
+        sentences.append((tokenized["input_ids"], is_claim))
+    sentence_embeddings = []
+    for sentence, is_claim in sentences:
+        word_embeddings = model(tensor([sentence]))["last_hidden_state"][0]
+        embeddings_sum = [sum(word_embeddings[i][j].item() for i in range(len(sentence))) for j in range(word_embeddings.size()[1])]
+        sentence_embeddings.append(([e / len(sentence) for e in embeddings_sum], is_claim))
+    return sentence_embeddings
