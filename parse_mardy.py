5c48a23e4e1217747c618f449a4f4f84b80071d6
Felix Eberhardt <feleber@yahoo.com>
2023-02-15 23:13:32 +0100

    fixed comments

--- parse_mardy.py
+++ parse_mardy.py
@@ -12,11 +12,7 @@ def parse_mardy_file(filename):
     for sent in data["sentences"]:
         begin = sent["begin"]
         end = sent["end"]
-        # sent = 5, 10
-        # claim could be: (2, 6), (8, 12), (6, 7), (3, 12)
-        # c[0] >= begin, c[0] <= end
-        # c[1] >= begin, c[1] <= end
-        # c[0] <= begin, c[1] >= end
+        # the sentence contains a claim if its range and that of a claim overlap
         is_claim = any(c[0] <= end and c[1] >= begin for c in claims)
         sentences.append((data["text"][begin:end], is_claim))
     return sentences
