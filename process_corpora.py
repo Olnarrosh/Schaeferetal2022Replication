a94214c89c2705c2dfb21a01952f6d362e4c8aa0
weickso <weicksa@web.de>
2023-02-22 13:04:59 +0100

    changed output location and now uses all parsers

--- process_corpora.py
+++ process_corpora.py
@@ -2,7 +2,7 @@
 @Author Tana Deeg and Sandro Weick
 """
 import parse_essay
-import preprocess, parse_cmv, parse_micro, parse_essay # parse_usdeb, parse_mardy
+import preprocess, parse_cmv, parse_micro, parse_essay , parse_usdeb, parse_mardy
 import random
 import pickle
 # get all tuple lists from the 5 corpora
@@ -17,9 +17,9 @@ def get_corpus_ready(corpus: str):
         case "essay":
             loadedcorpus = parse_essay.parse_essay_corpus()
         case "usdeb":
-            loadedcorpus = [] # TODO parse_usdeb()
+            loadedcorpus = parse_usdeb.parse_usdeb_corpus()
         case "mardy":
-            loadedcorpus = [] # TODO parse_mardy()
+            loadedcorpus = parse_mardy.parse_mardy_corpus()
         case "micro":
             loadedcorpus = parse_micro.parse_micro_corpus()
 
@@ -30,9 +30,9 @@ def get_corpus_ready(corpus: str):
     validate = finallist[int(len(finallist)*0.9):]
     print(len(validate))
 
-    with open(f"train_{corpus}_file.pkl", "wb") as file_train:
+    with open(f"./processed_data_results/train_{corpus}_file.pkl", "wb") as file_train:
         pickle.dump(train, file_train)
-    with open(f"val_{corpus}_file.pkl", "wb") as file_val:
+    with open(f"./processed_data_results/val_{corpus}_file.pkl", "wb") as file_val:
         pickle.dump(validate, file_val)
 
 # functionally equivalent to get_usdeb_ready(), but should work for all corpora
@@ -41,22 +41,22 @@ def get_leave_one_out(leave_out:str, corpora=["cmv", "essay", "micro"]):
     merged = []
     for corpus in corpora:
         if corpus != leave_out:
-            with open(f"train_{corpus}_file.pkl", "rb") as g:
+            with open(f"./processed_data_results/train_{corpus}_file.pkl", "rb") as g:
                 corp = pickle.load(g)
                 merged.extend(corp)
         
-    with open(f"train_without_{leave_out}_file.pkl", "wb") as file:
+    with open(f"./processed_data_results/train_without_{leave_out}_file.pkl", "wb") as file:
         pickle.dump(merged, file)
             
 # input should be eval split of corpus
 # output: list with gold values (0 or 1)
 def create_gold_list(corpus:str):
-    with open(f"val_{corpus}_file.pkl", "rb") as f:
+    with open(f"./processed_data_results/val_{corpus}_file.pkl", "rb") as f:
         file = pickle.load(f)
     goldlist  = []
     for i in file:
         goldlist.append(i[2])
-    with open(f"gold_{corpus}_list.pkl", "wb") as goldfile:
+    with open(f"./processed_data_results/gold_{corpus}_list.pkl", "wb") as goldfile:
         pickle.dump(goldlist, goldfile)
         
 
@@ -69,6 +69,6 @@ def process_all_corpora(corpora=["cmv", "essay", "mardy", "micro", "usdeb"]):
 
 if __name__ == "__main__":
 
-    process_all_corpora(["cmv", "essay", "micro"])
+    process_all_corpora()
 
 
