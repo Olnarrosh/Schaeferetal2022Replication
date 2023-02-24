2ca933ed92d09839fd5117d75688dd5e2b1e05ed
Felix Eberhardt <feleber@yahoo.com>
2023-02-24 02:13:29 +0100

    added regression function

--- linearregression.py
+++ linearregression.py
@@ -1,7 +1,60 @@
 
 import numpy as np
-
+from itertools import product, permutations
+from preprocess import compute_similarity
 from sklearn.linear_model import LinearRegression
+
+
+# takes two dicts as input, the first with corpus names (should be "cmv",
+# "usdeb", "essay", "mardy", "micro") as keys and the tokenized sentences
+# (use the tokenize function from preprocess) as values, the second with
+# tuples of corpora as keys and f-scores as values
+def regression(corpora: dict[str: list[float]], scores: dict[str: float]):
+    # compute all combinations of corpora (order doesn't matter)
+    corp_combos = product(corpora.keys(), corpora.keys())
+    corp_combos = set(tuple(sorted(x)) for x in corp_combos)
+    # compute similarity, corpus size, and claim ratio
+    sim = {cc: compute_similarity([corpora[c] for c in cc]) for cc in corp_combos}
+    size = {c: len(corpora[c]) for c in corpora}
+    claim_ratio = {c: len([s for s in corpora[c] if s[1]])/size[c] for c in corpora}
+    corpus = list(corpora.keys()) # use list to ensure fixed order
+    # compute independent variables for all corpus combinations
+    indep_vars = []
+    dep_vars = []
+    for cc in corp_combos:
+        order = _get_contained_ordering_(cc, scores.keys())
+        if not order:
+            if cc[0] == cc[1] and cc[0] in scores:
+                order = cc[0]
+            else:
+                continue
+        iv_vals = [sim[cc], size[cc[0]], claim_ratio[cc[0]], claim_ratio[cc[1]]]
+        iv_vals += [1 if i == corpus.index(cc[0]) else 0 for i in range(len(corpus))]
+        indep_vars.append(iv_vals)
+        dep_vars.append(scores[order])
+    # compute leave one out values
+    for c in corpus:
+        other = [x for x in corpus if c != x]
+        order = _get_contained_ordering_(other, scores.keys())
+        if not order:
+            continue
+        iv_vals = [compute_similarity(other),
+                   sum(size[o] for o in other),
+                   sum(len([s for s in corpora[o] if s[1] for o in other])) / sum(size(o) for o in other),
+                   ratio[c]]
+        iv_vals += [0 if i == corpus.index(c) else 1 for i in range(len(corpus))]
+        indep_vars.append(iv_vals)
+        dep_vars.append(scores[order])
+    return LinearRegression().fit(np.array(indep_vars), np.array(dep_vars)).coef_
+            
+
+# helper function so tuples in scores keys can be in any order
+def _get_contained_ordering_(permutable, container):
+    for p in permutations(permutable):
+        if p in container:
+            return p
+    return None
+
 
 #unabh. Variablen: spearman ähnlichkeit (3 kommastellen), Korpsugrößetraining(#Sätze), verhältnisclaimnichtclaimquell (anzahlclaims/anzahlsätzegesamt) auf 3 kommastellen, verhältnisclaimnichtclaimziel,
 # usdebtrain, microtrain, essaytrain, cmvtrain, mardytrain  (--> alle binary)
--- preprocess.py
+++ preprocess.py
@@ -5,22 +5,30 @@ from scipy.stats import spearmanr
 tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased")
 model = BertModel.from_pretrained("bert-base-multilingual-cased")
 
+# tokenize a sentence
+def tokenize(sentence: (str, bool)) -> (list[float], bool):
+    return (tokenizer(sentence[0])["input_ids"], sentence[1])
+
 # tokenize all sentences in the corpus, then extract and pool embeddings
 def convert_corpus(corpus: list[(str, bool)]) -> list[(str, list[float], bool)]:
     sentence_embeddings = []
     dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    for sentence, is_claim in corpus:
-        tokens = tokenizer(sentence)["input_ids"]
+    for sentence in corpus:
+        tokens, is_claim = tokenize(sentence)
         word_embeddings = model(torch.tensor([tokens], device=dev))["last_hidden_state"][0]
         embeddings_sum = [sum(word_embeddings[i][j].item() for i in range(len(tokens))) for j in range(word_embeddings.size()[1])]
         sentence_embeddings.append((sentence, [e / len(tokens) for e in embeddings_sum], is_claim))
     return sentence_embeddings
 
-# compute spearman similarity based on 500 most frequent words of each corpus
-def compute_similarity(corpora):
+# compute spearman similarity based on 500 most frequent words of each corpus.
+# should now work with both tokenized and untokenized corpora.
+def compute_similarity(corpora: list[list[tuple]]):
     top_types = []
     for corpus in corpora:
-        tokens = [tokenizer(sentence)["input_ids"] for sentence, _ in corpus]
+        if isinstance(corpus[0][0], str):
+            tokens = [tokenize(sentence)[0][1:-1] for sentence in corpus]
+        else:
+            tokens = [sentence[0][1:-1] for sentence in corpus]
         tokens = [token for sentence in tokens for token in sentence]
         types = set(tokens)
         freq = {t: tokens.count(t) for t in types}
