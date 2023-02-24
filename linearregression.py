5380cda86b2a5c40bc2ca47f240a97fdca674a5a
deegta <deeg.tana@gmail.com>
2023-02-24 17:01:24 +0100

    added missing values in regression, without spearman (function was changed now it doesn't work anymore)

--- linearregression.py
+++ linearregression.py
@@ -1,8 +1,11 @@
 
 import numpy as np
 from itertools import product, permutations
+
+import preprocess
 from preprocess import compute_similarity, tokenize
 from sklearn.linear_model import LinearRegression
+from scipy.stats import spearmanr
 import parse_cmv, parse_essay, parse_mardy, parse_micro, parse_usdeb
 
 
@@ -68,7 +71,8 @@ def _get_contained_ordering_(permutable, container):
 # also Länge pro liste ist 9
 # spearman: cmv essay 0.209, cmv micro 0.249, micro essay 0.302, usdeb cmv 0.211, usdeb essay 0.281, usdeb micro 0.206
 # Anzahl Listen = Anzahl Experimente: 5 + 20 + 5 = 30
-if __name__ == "__main__":
+
+    """
     corpora_dict = {
     "cmv": [sent.tokenize() for sent in parse_cmv],
     "usdeb":[sent.tokenize() for sent in parse_usdeb],
@@ -112,46 +116,61 @@ if __name__ == "__main__":
 
     regression(corpora_dict, scores_dict)
     
-    """
+    
+    # unabh. Variablen: spearman ähnlichkeit (3 kommastellen), Korpsugrößetraining(#Sätze), verhältnisclaimnichtclaimquell (anzahlclaims/anzahlsätzegesamt) auf 3 kommastellen, verhältnisclaimnichtclaimziel,
+    # usdebtrain, microtrain, essaytrain, cmvtrain, mardytrain  (--> alle binary)
+
+    # für leave one out: korpusgröße = summe aller größen, verhältnisclaims = summe aller claims / gesamtkorpusgröße
+
+    # also Länge pro liste ist 9
+    # spearman: cmv essay 0.209, cmv micro 0.249, micro essay 0.302, usdeb cmv 0.211, usdeb essay 0.281, usdeb micro 0.206
+    # Anzahl Listen = Anzahl Experimente: 5 + 20 + 5 = 30
                     # 5 in domain
                     # TODO: alle leave one out, alle spearman mit mardy, alle korpusgröße mardy, alle claimverhältnisse mardy
-    X = np.array([[1, 29621, 0.481, 0.481, 1, 0, 0, 0, 0],
-                  [1, 965, 0.035, 0.035, 0, 1, 0, 0, 0],
-                  [1, 1665, 0.305, 0.305, 0, 0, 1, 0, 0],
-                  [1, 2798, 0.366, 0.366, 0, 0, 0, 1, 0],
-                  [1, 0, 0, 0, 0, 0, 0, 0, 1],
+    X = np.array([[29621, 0.481, 0.481, 1, 0, 0, 0, 0],
+                  [965, 0.035, 0.035, 0, 1, 0, 0, 0],
+                  [1665, 0.305, 0.305, 0, 0, 1, 0, 0],
+                  [2798, 0.366, 0.366, 0, 0, 0, 1, 0],
+                  [18477, 0.048, 0.048, 0, 0, 0, 0, 1],
                   # 5 leave one out
-                  [0, 0, 0, 0, 0, 1, 1, 1, 1],
-                  [0, 0, 0, 0, 1, 0, 1, 1, 1],
-                  [0, 0, 0, 0, 1, 1, 0, 1, 1],
-                  [0, 0, 0, 0, 1, 1, 1, 0, 1],
-                  [0, 35049, 0.451, 0, 1, 1, 1, 1, 0],
+                  [23905, 0.103, 0.481, 0, 1, 1, 1, 1],
+                  [52561, 0.317, 0.035, 1, 0, 1, 1, 1],
+                  [51861, 0.312, 0.305, 1, 1, 0, 1, 1],
+                  [50728, 0.309, 0.366, 1, 1, 1, 0, 1],
+                  [35049, 0.451, 0.048, 1, 1, 1, 1, 0],
                   # 4 train usdeb
-                  [0.206, 29621, 0.481, 0.035, 1, 0, 0, 0, 0],
-                  [0.281, 29621, 0.481, 0.305, 1, 0, 0, 0, 0],
-                  [0.211, 29621, 0.481, 0.366, 1, 0, 0, 0, 0],
-                  [0, 29621, 0.481, 0, 1, 0, 0, 0, 0],
+                  [29621, 0.481, 0.035, 1, 0, 0, 0, 0],
+                  [29621, 0.481, 0.305, 1, 0, 0, 0, 0],
+                  [29621, 0.481, 0.366, 1, 0, 0, 0, 0],
+                  [29621, 0.481, 0.048, 1, 0, 0, 0, 0],
                   #4 train micro
-                  [0.206, 965, 0.035, 0.481, 0, 1, 0, 0, 0],
-                  [0.302, 965, 0.035, 0.305, 0, 1, 0, 0, 0],
-                  [0.249, 965, 0.035, 0.366, 0, 1, 0, 0, 0],
-                  [0, 965, 0.035, 0, 0, 1, 0, 0, 0],
+                  [965, 0.035, 0.481, 0, 1, 0, 0, 0],
+                  [965, 0.035, 0.305, 0, 1, 0, 0, 0],
+                  [965, 0.035, 0.366, 0, 1, 0, 0, 0],
+                  [965, 0.035, 0.048, 0, 1, 0, 0, 0],
                   # 4 train essay
-                  [0.281, 1665, 0.305, 0.481, 0, 0, 1, 0, 0],
-                  [0.302, 1665, 0.305, 0.035, 0, 0, 1, 0, 0],
-                  [0.209, 1665, 0.305, 0.366, 0, 0, 1, 0, 0],
-                  [0, 1665, 0.305, 0, 0, 0, 1, 0, 0],
+                  [1665, 0.305, 0.481, 0, 0, 1, 0, 0],
+                  [1665, 0.305, 0.035, 0, 0, 1, 0, 0],
+                  [1665, 0.305, 0.366, 0, 0, 1, 0, 0],
+                  [1665, 0.305, 0.048, 0, 0, 1, 0, 0],
                   #4 train cmv
-                  [0.211, 2798, 0.366, 0.481, 0, 0, 0, 1, 0],
-                  [0.249, 2798, 0.366, 0.035, 0, 0, 0, 1, 0],
-                  [0.209, 2798, 0.366, 0.305, 0, 0, 0, 1, 0],
-                  [0, 2798, 0.366, 0, 0, 0, 0, 1, 0],
+                  [2798, 0.366, 0.481, 0, 0, 0, 1, 0],
+                  [2798, 0.366, 0.035, 0, 0, 0, 1, 0],
+                  [2798, 0.366, 0.305, 0, 0, 0, 1, 0],
+                  [2798, 0.366, 0.048, 0, 0, 0, 1, 0],
                   # 4 train mardy
-                  [0, 0, 0, 0.481, 0, 0, 0, 0, 1],
-                  [0, 0, 0, 0.035, 0, 0, 0, 0, 1],
-                  [0, 0, 0, 0.305, 0, 0, 0, 0, 1],
-                  [0, 0, 0, 0.366, 0, 0, 0, 0, 1]])
-    y = [] #TODO insert all F-Scores, auf 3 kommastellen runden
+                  [18477, 0.048, 0.481, 0, 0, 0, 0, 1],
+                  [18477, 0.048, 0.035, 0, 0, 0, 0, 1],
+                  [18477, 0.048, 0.305, 0, 0, 0, 0, 1],
+                  [18477, 0.048, 0.366, 0, 0, 0, 0, 1]])
+
+    y = [0.69, 0.83, 0.47, 0.63, 0,
+         0.37, 0.18, 0.45, 0.4, 0,
+         0.16, 0.55, 0.53, 0.06,
+         0.0014, 0, 0, 0,
+         0.25, 0.19, 0.35, 0,
+         0.43, 0.09, 0.29, 0,
+         0, 0, 0, 0] #TODO insert all F-Scores, auf 3 kommastellen runden
     reg = LinearRegression().fit(X, y)
     # prints all weights -> see which independent variables are most important!!
     print(reg.coef_)
