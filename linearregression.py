ded8ab1610de2534c827a138b3d139122a6cc917
deegta <deeg.tana@gmail.com>
2023-02-27 13:48:25 +0100

    Fixed comments etc for submission

--- linearregression.py
+++ linearregression.py
@@ -3,6 +3,7 @@ import numpy as np
 from itertools import product, permutations
 from preprocess import compute_similarity, tokenize
 from sklearn.linear_model import LinearRegression
+from math import log
 import parse_cmv, parse_essay, parse_mardy, parse_micro, parse_usdeb
 
 
@@ -17,10 +18,8 @@ def regression(corpora: dict[str: list[float]], scores: dict[str: float]):
     corp_combos = product(corpora.keys(), corpora.keys())
     corp_combos = set(tuple(sorted(x)) for x in corp_combos)
     
-    print("now computes similaritys")
     # compute similarity, corpus size, and claim ratio
-    sim = {cc: compute_similarity([corpora[c] for c in cc]) for cc in corp_combos}
-    print("done with similaritys")
+    sim = {cc: 1 if cc[0] == cc[1] else compute_similarity([corpora[c] for c in cc]) for cc in corp_combos}
     size = {c: len(corpora[c]) for c in corpora}
     claim_ratio = {c: len([s for s in corpora[c] if s[1]])/size[c] for c in corpora}
     corpus = list(corpora.keys()) # use list to ensure fixed order
@@ -34,21 +33,23 @@ def regression(corpora: dict[str: list[float]], scores: dict[str: float]):
                 order = cc[0]
             else:
                 continue
-        iv_vals = [sim[cc], size[cc[0]], claim_ratio[cc[0]], claim_ratio[cc[1]]]
+        iv_vals = [sim[cc], log(size[cc[0]]), claim_ratio[cc[0]], claim_ratio[cc[1]]]
         iv_vals += [1 if i == corpus.index(cc[0]) else 0 for i in range(len(corpus))]
         indep_vars.append(iv_vals)
         dep_vars.append(scores[order])
     # compute leave one out values
-    print("now at leave one out")
     for c in corpus:
         other = [x for x in corpus if c != x]
         order = _get_contained_ordering_(other, scores.keys())
         if not order:
             continue
-        iv_vals = [compute_similarity(other),
-                   sum(size[o] for o in other),
-                   sum(len([s for s in corpora[o] if s[1] for o in other])) / sum(size(o) for o in other),
-                   ratio[c]]
+        other_combos = product(other, other)
+        other_combos = set(tuple(sorted(x)) for x in other_combos if x[0] != x[1])
+        print(other_combos)
+        iv_vals = [sum(sim[o] for o in other_combos) / len(other),
+                   sum(log(size[o]) for o in other),
+                   sum(len([s for s in corpora[o] if s[1]]) for o in other) / sum(size[o] for o in other),
+                   claim_ratio[c]]
         iv_vals += [0 if i == corpus.index(c) else 1 for i in range(len(corpus))]
         indep_vars.append(iv_vals)
         dep_vars.append(scores[order])
@@ -65,10 +66,6 @@ def _get_contained_ordering_(permutable, container):
 
 
 if __name__ == "__main__":
-    mardy = parse_mardy.parse_mardy_corpus()
-    sub_mardy = []
-    for el in mardy:
-        sub_mardy.extend(el)
 
     corpora_dict = {
     "cmv": [tokenize(sent) for sent in parse_cmv.parse_cmv_corpus()],
@@ -77,8 +74,6 @@ if __name__ == "__main__":
     "essay":[tokenize(sent) for sent in parse_essay.parse_essay_corpus()],
     "mardy":[tokenize(sent) for sent in parse_mardy.parse_mardy_corpus()]
     }
-
-    print("now at scores dict")
 
     # f-score results from Linear Regression Model
     scores_dict = {
@@ -113,8 +108,6 @@ if __name__ == "__main__":
         ("cmv", "micro", "essay", "mardy"): 0.37,
         ("usdeb", "micro", "essay", "mardy"): 0.4
         }
-    
-    print("now at regression")
 
     print(regression(corpora_dict, scores_dict))
 
